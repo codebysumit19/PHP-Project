83fa51678b10b0bf6339a8b7afef4c9d2667742a
Sumit Kumar <sumit.kumar@solutionara.com>
2025-12-23 11:53:19 +0530

    Initial commit

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
+//nwscmn
 $currentPage = basename($_SERVER['PHP_SELF']);  // e.g. "link.php"
 
 $headerTitle = isset($pageTitle) && $pageTitle !== ''
